c017901a936d93a6bad7643a65a6d0e8558c25ab
vinylflamingo <fcostoyaprograms@gmail.com>
2020-06-24 22:49:53 -0400

    setup project dependency injection

---
+++ UfcElo.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UfcElo.Data.Services;
+
+namespace UfcElo.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryBoutData>().As<IBoutData>().SingleInstance() ;
+            builder.RegisterType<InMemoryFighterData>().As<IFighterData>().SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+        }
+    }
+}
--- UfcElo.Web/Global.asax.cs
+++ UfcElo.Web/Global.asax.cs
@@ -16,6 +16,8 @@ namespace UfcElo.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ContainerConfig.RegisterContainer();
         }
     }
 }
+                                                          
